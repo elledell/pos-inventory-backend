96887b9427f0a572d409cf4d4795b05eb59f4109
elledell <hellenmochama7@gmail.com>
2026-04-06 10:09:08 +0300

    Fix timezone to Africa/Nairobi

--- src/main/java/com/inventory1/system/Application.java
+++ src/main/java/com/inventory1/system/Application.java
@@ -2,12 +2,20 @@ package com.inventory1.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		// This forces Spring Boot to always run in Nairobi time (EAT)
+		TimeZone.setDefault(TimeZone.getTimeZone("Africa/Nairobi"));
 	}
 
 }
